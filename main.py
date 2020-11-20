f7ec6e459dd99600d45d6c5fae6ccbc7c7f41d06
Zhihong Li <zhihongli@bennington.edu>
2020-11-19 20:17:49 -0500

    beautify the UI

--- main.py
+++ main.py
@@ -251,7 +251,7 @@ def account():
                 return render_template("account.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=False)
+    app.run(host="127.0.0.1", debug=False, port=8000)
 
 
 """
